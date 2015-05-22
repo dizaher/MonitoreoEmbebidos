dd57338d114c324a906aa5b25dd77edfd54346ed
dizaher <zadh11@gmail.com>
2015-05-22 13:46:56 -0500

    Cambios en graficos
    se modificaron los titulos y se esta empezando a configurar la variables
    de tiempo real del grafico

--- application/views/Acuaponia/graphs_acuaponia_view.php
+++ application/views/Acuaponia/graphs_acuaponia_view.php
@@ -64,14 +64,7 @@
                     <div class="box box-primary">
                       <div class="box-header">
                         <i class="fa fa-bar-chart-o"></i>
-                        <h3 class="box-title">Interactive Area Chart</h3>
-                        <div class="box-tools pull-right">
-                          Real time
-                          <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                            <button type="button" class="btn btn-default btn-xs active" data-toggle="on">On</button>
-                            <button type="button" class="btn btn-default btn-xs" data-toggle="off">Off</button>
-                          </div>
-                        </div>
+                        <h3 class="box-title">Lecturas en tiempo real</h3>                        
                       </div>
                       <div class="box-body">
                         <div id="interactive" style="height: 300px;"></div>
--- application/views/Calentador/graphs_calentador_view.php
+++ application/views/Calentador/graphs_calentador_view.php
@@ -64,14 +64,7 @@
                     <div class="box box-primary">
                       <div class="box-header">
                         <i class="fa fa-bar-chart-o"></i>
-                        <h3 class="box-title">Interactive Area Chart</h3>
-                        <div class="box-tools pull-right">
-                          Real time
-                          <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                            <button type="button" class="btn btn-default btn-xs active" data-toggle="on">On</button>
-                            <button type="button" class="btn btn-default btn-xs" data-toggle="off">Off</button>
-                          </div>
-                        </div>
+                        <h3 class="box-title">Lecturas en tiempo real</h3>                        
                       </div>
                       <div class="box-body">
                         <div id="interactive" style="height: 300px;"></div>
@@ -125,24 +118,15 @@
     <script src="<?php echo base_url(); ?>js/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
         
     <script type="text/javascript">
-
-      $(function () {
-
-        /*
-         * Flot Interactive Chart
-         * -----------------------
-         */
+      $(function () {        
         // We use an inline data source in the example, usually data would
         // be fetched from a server
         var data = [], totalPoints = 100;
         function getRandomData() {
-
           if (data.length > 0)
             data = data.slice(1);
-
           // Do a random walk
           while (data.length < totalPoints) {
-
             var prev = data.length > 0 ? data[data.length - 1] : 50,
                     y = prev + Math.random() * 10 - 5;
 
@@ -151,19 +135,16 @@
             } else if (y > 100) {
               y = 100;
             }
-
             data.push(y);
           }
-
           // Zip the generated y values with the x values
           var res = [];
           for (var i = 0; i < data.length; ++i) {
             res.push([i, data[i]]);
           }
-
           return res;
         }
-
+        
         var interactive_plot = $.plot("#interactive", [getRandomData()], {
           grid: {
             borderColor: "#f3f3f3",
@@ -193,43 +174,14 @@
         function update() {
 
           interactive_plot.setData([getRandomData()]);
-
           // Since the axes don't change, we don't need to call plot.setupGrid()
           interactive_plot.draw();
           if (realtime === "on")
             setTimeout(update, updateInterval);
         }
-
         //INITIALIZE REALTIME DATA FETCHING
-        if (realtime === "on") {
-          update();
-        }
-        //REALTIME TOGGLE
-        $("#realtime .btn").click(function () {
-          if ($(this).data("toggle") === "on") {
-            realtime = "on";
-          }
-          else {
-            realtime = "off";
-          }
-          update();
-        });
-        /*
-         * END INTERACTIVE CHART
-         */  
-
-      });
-
-      /*
-       * Custom Label formatter
-       * ----------------------
-       */
-      function labelFormatter(label, series) {
-        return "<div style='font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;'>"
-                + label
-                + "<br/>"
-                + Math.round(series.percent) + "%</div>";
-      }
+        update();                
+      });      
     </script>
     </body>
 </html>
